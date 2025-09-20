f304c438a4822aab9d89bb2179a16ee91dccf78c
Evokoo <evanjohncooper@gmail.com>
2025-09-20 19:56:53 +1000

    Day 07 complete

--- 07/07.go
+++ 07/07.go
@@ -1,5 +1,65 @@
 package day07
-	
-	func Solve(file string) int {
-		return 0
+
+import (
+	"math"
+	"strconv"
+	"strings"
+
+	"github.com/Evokoo/AOC_2022_Go/tools"
+)
+
+type FilePath []int
+
+func (fp *FilePath) push(dir int) {
+	*fp = append(*fp, dir)
+}
+func (fp *FilePath) pop() {
+	*fp = (*fp)[:len(*fp)-1]
+}
+
+func Solve(file string, part int) int {
+	data := tools.ReadFile(file)
+	dirs := traverseFileTree(data)
+
+	dir := math.MaxInt
+	sum := 0
+
+	for _, size := range dirs {
+		if size <= 100000 {
+			sum += size
+		}
+
+		if (70000000-dirs[0])+size >= 30000000 && size < dir {
+			dir = size
+		}
 	}
+	if part == 2 {
+		return dir
+	}
+	return sum
+}
+func traverseFileTree(data string) map[int]int {
+	var path FilePath
+	var id int
+
+	volumes := make(map[int]int)
+
+	for command := range strings.SplitSeq(data, "\n") {
+		c := strings.Fields(command)
+
+		if c[0] == "$" && c[1] == "cd" {
+			if c[2] == ".." {
+				path.pop()
+			} else {
+				path.push(id)
+				id++
+			}
+		}
+		if size, err := strconv.Atoi(c[0]); err == nil {
+			for _, dir := range path {
+				volumes[dir] += size
+			}
+		}
+	}
+	return volumes
+}
--- 07/07_test.go
+++ 07/07_test.go
@@ -15,17 +15,17 @@ type Test struct {
 }
 
 var tests = []Test{
-	// {part: 1, file: "example.txt", target: -1},
-	// {part: 1, file: "input.txt", target: -1},
-	// {part: 2, file: "example.txt", target: -1},
-	// {part: 2, file: "input", target: -1},
+	{part: 1, file: "example.txt", target: 95437},
+	{part: 1, file: "input.txt", target: 1581595},
+	{part: 2, file: "example.txt", target: 24933642},
+	{part: 2, file: "input.txt", target: 1544176},
 }
 
 var _ = Describe("AOC 2022 - Day 07", func() {
 	for _, test := range tests {
 		msg := fmt.Sprintf("Testing Part %d with %s", test.part, test.file)
 		It(msg, func() {
-			result := Solve(test.file)
+			result := Solve(test.file, test.part)
 			Expect(result).To(Equal((test.target)))
 		})
 	}
