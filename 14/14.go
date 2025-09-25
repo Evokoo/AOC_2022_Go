749415fd24c7925f35a3a1babbefdbccaaa1f8c4
Evokoo <evanjohncooper@gmail.com>
2025-09-26 01:10:40 +1000

    Day 14 complete

--- 14/14.go
+++ 14/14.go
@@ -1,7 +1,6 @@
 package day14
 
 import (
-	"fmt"
 	"strconv"
 	"strings"
 
@@ -9,20 +8,27 @@ import (
 	"github.com/Evokoo/AOC_2022_Go/tools/ds"
 )
 
-func Solve(file string) int {
-	cave := parseInput(file)
+type Point struct{ x, y int }
+type Sand = Point
+type Cave = ds.Set[Point]
 
-	fmt.Println(cave)
-	fmt.Println(len(cave))
-	return 0
+func Solve(file string, part int) int {
+	cave, depth := parseInput(file)
+
+	for i := 0; ; i++ {
+		grain := newParticle()
+		if !cave.Has(grain) && grain.fall(cave, depth, part) {
+			cave.Add(grain)
+		} else {
+			return i
+		}
+	}
 }
 
-type Point struct{ x, y int }
-type Cave ds.Set[Point]
-
-func parseInput(file string) Cave {
+func parseInput(file string) (Cave, int) {
 	data := tools.ReadFile(file)
 	cave := ds.NewSet[Point]()
+	maxY := 0
 
 	for line := range strings.SplitSeq(data, "\n") {
 		coords := strings.Split(line, " -> ")
@@ -44,15 +50,50 @@ func parseInput(file string) Cave {
 					current.y--
 				}
 				cave.Add(current)
+
+				if current.y > maxY {
+					maxY = current.y
+				}
 			}
 			current = target
 		}
 	}
-	return Cave(cave)
+	return Cave(cave), maxY
 }
 func coordToPoint(s string) Point {
 	digits := tools.QuickMatch(s, `\d+`)
 	x, _ := strconv.Atoi(digits[0])
 	y, _ := strconv.Atoi(digits[1])
 	return Point{x, y}
+}
+
+func newParticle() Sand {
+	return Sand{500, 0}
+}
+func (s *Sand) fall(cave Cave, maxDepth int, part int) bool {
+	for {
+		if part == 2 && s.y+1 == (maxDepth+2) {
+			return true
+		} else {
+			if s.y > maxDepth {
+				return false
+			}
+		}
+		down := Sand{s.x, s.y + 1}
+		left := Sand{s.x - 1, s.y + 1}
+		right := Sand{s.x + 1, s.y + 1}
+
+		switch {
+		case !cave.Has(down):
+			s.y++
+		case !cave.Has(left):
+			s.x--
+			s.y++
+		case !cave.Has(right):
+			s.x++
+			s.y++
+		default:
+			return true
+		}
+	}
 }
--- 14/14_test.go
+++ 14/14_test.go
@@ -15,17 +15,17 @@ type Test struct {
 }
 
 var tests = []Test{
-	{part: 1, file: "example.txt", target: -1},
-	// {part: 1, file: "input.txt", target: -1},
-	// {part: 2, file: "example.txt", target: -1},
-	// {part: 2, file: "input.txt", target: -1},
+	{part: 1, file: "example.txt", target: 24},
+	{part: 1, file: "input.txt", target: 592},
+	{part: 2, file: "example.txt", target: 93},
+	{part: 2, file: "input.txt", target: 30367},
 }
 
 var _ = Describe("AOC 2022 - Day 14", func() {
 	for _, test := range tests {
 		msg := fmt.Sprintf("Testing Part %d with %s", test.part, test.file)
 		It(msg, func() {
-			result := Solve(test.file)
+			result := Solve(test.file, test.part)
 			Expect(result).To(Equal((test.target)))
 		})
 	}
