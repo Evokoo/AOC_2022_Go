6a61cf396dd82d423c79602a414c9c56abf380ce
Evokoo <evanjohncooper@gmail.com>
2025-09-26 22:59:56 +1000

    Absolute and Manhattan Distanced added

--- tools/tools.go
+++ tools/tools.go
@@ -5,6 +5,8 @@ import (
 	"regexp"
 )
 
+type Point struct{ X, Y int }
+
 func ReadFile(title string) string {
 	data, err := os.ReadFile(title)
 	if err != nil {
@@ -16,4 +18,14 @@ func ReadFile(title string) string {
 func QuickMatch(str, pattern string) []string {
 	re := regexp.MustCompile(pattern)
 	return re.FindAllString(str, -1)
+}
+
+func ManhattanDistance(a Point, b Point) int {
+	return Abs(a.X-b.X) + Abs(a.Y-b.Y)
+}
+func Abs(n int) int {
+	if n < 0 {
+		return -n
+	}
+	return n
 }
