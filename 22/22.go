a2e1419db997ebfb5c143193c8d3c8fc54c074bf
Evokoo <evanjohncooper@gmail.com>
2025-10-07 22:48:13 +1100

    Day 22 complete

--- 22/22.go
+++ 22/22.go
@@ -1,21 +1,11 @@
 package day22
 
-<<<<<<< HEAD
-func Solve(file string) int {
-	grid, moves := ParseInput(file)
-	state := NewState(grid.start)
-
-	for i, move := range moves {
-		state.UpdateState(move, grid, i)
-=======
 func Solve(file string, part int) int {
 	grid, moves, state := ParseInput(file, part)
 
 	for _, move := range moves {
 		state.UpdateState(move, grid)
->>>>>>> day-22-debug
 	}
 
 	return state.GetPassword()
-
 }
